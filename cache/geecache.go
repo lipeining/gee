c53bd2a1d43811b5da2c5d7d22605547baaf0eac
lipeining <827154031@qq.com>
2024-08-26 17:27:17 +0800

    feat: add sigle flight

--- cache/geecache.go
+++ cache/geecache.go
@@ -1,6 +1,7 @@
 package cache
 
 import (
+	"cache/singleflight"
 	"fmt"
 	"log"
 	"sync"
@@ -25,6 +26,9 @@ type Group struct {
 	getter    Getter
 	mainCache cache
 	peers     PeerPicker
+	// use singleflight.Group to make sure that
+	// each key is only fetched once
+	loader *singleflight.Group
 }
 
 var (
@@ -45,6 +49,7 @@ func NewGroup(name string, cacheBytes int64, getter Getter) *Group {
 		name:      name,
 		getter:    getter,
 		mainCache: cache{cacheBytes: cacheBytes},
+		loader:    &singleflight.Group{},
 	}
 
 	groups[name] = g
@@ -84,16 +89,25 @@ func (g *Group) Get(key string) (ByteView, error) {
 }
 
 func (g *Group) load(key string) (value ByteView, err error) {
-	if g.peers != nil {
-		if peer, ok := g.peers.PickPeer(key); ok {
-			if value, err = g.getFromPeer(peer, key); err == nil {
-				return value, nil
+	// each key is only fetched once (either locally or remotely)
+	// regardless of the number of concurrent callers.
+	viewi, err := g.loader.Do(key, func() (interface{}, error) {
+		if g.peers != nil {
+			if peer, ok := g.peers.PickPeer(key); ok {
+				if value, err = g.getFromPeer(peer, key); err == nil {
+					return value, nil
+				}
+				log.Println("[GeeCache] Failed to get from peer", err)
 			}
-			log.Println("[GeeCache] Failed to get from peer", err)
 		}
-	}
 
-	return g.getLocally(key)
+		return g.getLocally(key)
+	})
+
+	if err == nil {
+		return viewi.(ByteView), nil
+	}
+	return
 }
 
 func (g *Group) getFromPeer(peer PeerGetter, key string) (ByteView, error) {
--- cache/go.mod
+++ cache/go.mod
@@ -1,3 +1,8 @@
 module cache
 
 go 1.22.6
+
+require (
+	github.com/golang/protobuf v1.5.4 // indirect
+	google.golang.org/protobuf v1.34.2 // indirect
+)
